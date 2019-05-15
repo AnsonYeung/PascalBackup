9a90da1e51bb690840136ec5cb196d7e9d018f28
AnsonYeung <s151204@tanghin.edu.hk>
2019-05-15 15:39:27 +0800

    insertion sort

--- 2019-5/sort.pas
+++ 2019-5/sort.pas
@@ -2,11 +2,18 @@ Program sort;
 Uses Sysutils;
 Const size = 5;
 Var list : Array[1..size] Of Integer;
-	i, pass: Integer;
+	i: Integer;
+
+Procedure SwapInt(Var a, b: Integer);
+Var tmp : Integer;
+Begin
+	tmp := a;
+	a := b;
+	b := tmp;
+End;
 
 Procedure bubblesort();
-Var i: Integer;
-	tmp: Integer;
+Var i, pass: Integer;
 	flag: Boolean;
 Begin
 	pass := size - 1;
@@ -18,25 +25,64 @@ Begin
 			If list[i] > list[i + 1] Then
 			Begin
 				flag := True;
-				tmp := list[i];
-				list[i] := list[i + 1];
-				list[i + 1] := tmp;
+				SwapInt(list[i], list[i + 1]);
 			End;
 		Dec(pass);
 	End;
 End;
 
+Procedure selectionsort();
+Var largestLoc: Integer;
+	i, pass: Integer;
 Begin
-	list[1] := 18;
-	list[2] := 16;
-	list[3] := 19;
-	list[4] := 17;
-	list[5] := 15;
-	if size in [1,5] Then
+	For pass := size DownTo 2 Do
 	Begin
-		writeln('test');
+		largestLoc := 1;
+		For i := 2 To pass Do
+			If list[largestLoc] < list[i] Then
+				largestLoc := i;
+		SwapInt(list[largestLoc], list[pass]);
 	End;
-	bubblesort;
+End;
+
+Procedure selectionsort2();
+Var smallestLoc: Integer;
+	i, pass: Integer;
+Begin
+	For pass := 1 to size - 1 Do
+	Begin
+		smallestLoc := pass;
+		For i := pass + 1 To size Do
+			If list[smallestLoc] > list[i] Then
+				smallestLoc := i;
+		SwapInt(list[smallestLoc], list[pass]);
+	End;
+End;
+
+Procedure insertionsort();
+Var currentNum, i, pass: Integer;
+Begin
+	For pass := 2 To size Do
+	Begin
+		currentNum := list[pass];
+		i := pass;
+		While (i > 1) And (list[i - 1] > currentNum) Do
+		Begin
+			list[i] := list[i - 1];
+			i := i - 1;
+		End;
+		list[i] := currentNum;
+	End;
+End;
+
+Begin
+	randomize();
+	list[1] := random(10);
+	list[2] := random(10);
+	list[3] := random(10);
+	list[4] := random(10);
+	list[5] := random(10);
+	insertionsort();
 
 	for i := 1 To Size Do
 		writeLn(list[i]);
